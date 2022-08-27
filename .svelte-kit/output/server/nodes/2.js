addc4f905498c6db27a46bac17316d522ad23526
Compey <hi@devcomp.tk>
2022-08-28 00:03:49 +0530

    why

--- .svelte-kit/output/server/nodes/2.js
+++
@@ -1,7 +0,0 @@
-import * as module from '../entries/pages/index.svelte.js';
-
-export { module };
-export const index = 2;
-export const file = '_app/immutable/pages/index.svelte-09d9d0a4.js';
-export const imports = ["_app/immutable/pages/index.svelte-09d9d0a4.js","_app/immutable/chunks/index-01f0dac0.js","_app/immutable/chunks/index-c7850084.js"];
-export const stylesheets = [];
